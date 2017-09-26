54b9e9e59b396f3bb7edaaccb068ce637015b68f
keposet <jsn.hutson@gmail.com>
2017-09-26 10:24:49 -0600

    Initial commit

---
+++ app/src/main/java/com/example/jason/firstapp/MyWorker.java
@@ -0,0 +1,13 @@
+package com.example.jason.firstapp;
+
+/**
+ * Created by jason on 9/25/2017.
+ */
+
+public class MyWorker {
+    public static int doubleTheValue(int initialValue){
+        int doubledValue = 2* initialValue;
+
+        return doubledValue;
+    }
+}
